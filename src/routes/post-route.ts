cd50a9ab9de323364dfbe8de12f6fd985458e16d
Wildan Rahadian <ryan7300@gmail.com>
2026-01-05 18:38:27 +0700

    Update post-route.ts

--- src/routes/post-route.ts
+++ src/routes/post-route.ts
@@ -1,11 +1,11 @@
 import express from 'express'
-import { getPosts, createPosts, updatePost, deletePost } from "../controllers/post-controller";
+import { getProducts, createProducts, updateProduct, deleteProduct } from "../controllers/post-controller";
 
 const router = express.Router();
 
-router.get('/posts', getPosts);
-router.post('/posts', createPosts);
-router.put("/posts/:id", updatePost);
-router.delete("/posts/:id", deletePost);
+router.get('/products', getProducts);
+router.post('/products', createProducts);
+router.put("/products/:id", updateProduct);
+router.delete("/products/:id", deleteProduct);
 
 export default router;
