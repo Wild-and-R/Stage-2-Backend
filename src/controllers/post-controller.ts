364765df018137da1e6b03f006d46268e90a6ab9
Wildan Rahadian <ryan7300@gmail.com>
2026-01-05 18:16:25 +0700

    Update post-controller.ts

--- src/controllers/post-controller.ts
+++ src/controllers/post-controller.ts
@@ -15,3 +15,24 @@ export const createPosts = (req: Request, res: Response) => {
     posts.push(newPost);
     res.status(201).json(newPost);
 };
+
+export const updatePost = (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    const { title, content } = req.body;
+
+    const post = posts.find(p => p.id === id);
+    if (post === undefined) {
+        return res.status(404).json({ message: "Post not found" });
+    }
+    post.title = title ?? post.title;
+    post.content = content ?? post.content;
+
+    res.json(post);
+};
+
+export const deletePost = (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    const index = posts.findIndex(p => p.id === id);
+    const deletedPost = posts.splice(index, 1);
+    res.json({ message: "Post deleted", post: deletedPost[0] });
+};
